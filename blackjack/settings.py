6997925e4a1647a2d1671ab132067ba69bd35f54
Daniel-G-91 <gavrilescu.daniel91@gmail.com>
2024-10-19 20:42:33 +0300

    Adde LOGGING to settings.py

--- blackjack/settings.py
+++ blackjack/settings.py
@@ -126,3 +126,22 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/home/gavrilescu_daniel91/blackjack_cardgame/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
